dd17de21a5ea34638695d0e5faef5cbae14388c0
rifat3790 <mdrifayethossen@gmail.com>
2023-08-14 23:07:54 +0600

    deposite complete

--- js/deposit.js
+++ js/deposit.js
@@ -1,9 +1,35 @@
 document.getElementById('btn-deposite').addEventListener('click', function(){
     const depositeField = document.getElementById('deposit-field');
-    const depositAmount = depositeField.value;
+    const newDepositAmountString = depositeField.value;
+    
+    const newDepositAmount = parseFloat(newDepositAmountString);
     
     // get the deposit current amount
     const depositTotalElement = document.getElementById('deposit-total');
-    const depositTotal = depositTotalElement.innerText;
-    depositTotalElement.innerText = depositAmount;
+    const perviousDepositTotalString = depositTotalElement.innerText;
+
+    const perviousDepositTotal = parseFloat(perviousDepositTotalString);
+
+    const currentDepositeAmount = perviousDepositTotal + newDepositAmount;
+
+    depositTotalElement.innerText = currentDepositeAmount;
+
+
+    // get blance
+    const blanceTotalElement = document.getElementById('blance-total');
+
+    const perviousBlanceTotalString = blanceTotalElement.innerText;
+
+    const perviousBlanceTotal = parseFloat(perviousBlanceTotalString);
+
+    const currentBlanceTotal = perviousBlanceTotal + newDepositAmount;
+
+    blanceTotalElement.innerText = currentBlanceTotal;
+
+
+    // clear the deposite field
+    depositeField.value = '';
+
+
+
 })
